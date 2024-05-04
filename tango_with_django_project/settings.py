27d3d4fe23543f9a902fd08e8890e231675c28a1
glasgwegian <2867781p@student.gla.ac.uk>
2024-05-04 23:14:37 +0100

    Finished chapter 11

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
+    'registration',
 ]
 
 MIDDLEWARE = [
@@ -129,4 +130,7 @@ MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
 # Authentication
-LOGIN_URL = 'rango:login'
+REGISTRATION_OPEN = True
+REGISTRATION_AUTO_LOGIN = True
+LOGIN_REDIRECT_URL = 'rango:index'
+LOGIN_URL = 'auth_login'
